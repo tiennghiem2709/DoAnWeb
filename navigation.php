c5a5d874f9429b32e3cf3bd635d7f919c80b59d9
ThanhTrieu184 <huynhthanhtrieu184@gmail.com>
2020-03-25 20:21:05 +0700

    code demo giao dien

---
+++ navigation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<style>
+		.navcustom{
+			height: auto;
+			background-color: #607d8b;
+			background-image: linear-gradient(#90a4ae,#455a64); 
+			margin-bottom: 30px; 
+			padding-bottom: 50px;
+		}
+	</style>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark navcustom ">
+<div class="container">
+  <a class="navbar-brand" href="#">CIMEMA'S LAND</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+	<ul class="navbar-nav ml-auto">
+	  <li class="nav-item active">
+		<a class="nav-link" href="#">TRANG CHỦ</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="#">PHIM</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="#">RẠP</a>
+	  </li>
+	  <li class="nav-item">
+		<a class="nav-link" href="#">THÀNH VIÊN</a>
+	  </li>
+	</ul>
+  </div>
+</div>
+</nav>
+</body>
